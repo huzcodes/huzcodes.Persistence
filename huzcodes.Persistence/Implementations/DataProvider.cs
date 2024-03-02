4af7b3766c06f0f214c8d6ddc5a3e657cf4da0f4
huzcodes <huzcodes@outlook.com>
2024-03-02 22:48:48 +0400

    updating the implementation for the Uparameter

--- huzcodes.Persistence/Implementations/DataProvider.cs
+++ huzcodes.Persistence/Implementations/DataProvider.cs
@@ -3,8 +3,6 @@ using Microsoft.Extensions.Configuration;
 
 namespace huzcodes.Persistence.Implementations
 {
-    //TODO thinking about the name, leave it like that or change it
-    // other options IServiceProvider, IPersistenceServices
     public partial class DataProvider(IConfiguration configuration) : IDataProvider
     {
         private readonly IConfiguration _configuration = configuration;
