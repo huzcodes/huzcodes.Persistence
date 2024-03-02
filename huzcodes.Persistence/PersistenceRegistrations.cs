bb70c8d025b1e9f92abfa78162dba9c6e8821d79
huzcodes <huzcodes@outlook.com>
2024-03-02 18:38:14 +0400

    adding service registration for the persistence nuget package with summary and the ability to change the service lifetime

--- huzcodes.Persistence/PersistenceRegistrations.cs
+++ huzcodes.Persistence/PersistenceRegistrations.cs
@@ -1,7 +1,34 @@
-﻿namespace huzcodes.Persistence
+﻿using huzcodes.Persistence.Implementations;
+using huzcodes.Persistence.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace huzcodes.Persistence
 {
     public static class PersistenceRegistrations
     {
-        //TODO adding registration function to register the package
+        /// <summary>
+        /// This function to be called in the program cs class,
+        /// for adding registration for the persistence services.
+        /// Note: the service life time by default is scoped,
+        /// and in case you want to change the service life time, 
+        /// you can pass the int paramter serviceLifetime.
+        /// </summary>
+        /// <param name="serviceLifetime">parameter to define the service life time by deafult scoped
+        /// ex: (int)ServiceLifetime.Scoped, (int)ServiceLifetime.Transient, (int)ServiceLifetime.Singleton</param>
+        public static void AddPersistenceServices(this IServiceCollection services, int serviceLifetime = (int)ServiceLifetime.Scoped)
+        {
+            switch (serviceLifetime)
+            {
+                case (int)ServiceLifetime.Transient:
+                    services.AddTransient<IDataProvider, DataProvider>();
+                    break;
+                case (int)ServiceLifetime.Scoped:
+                    services.AddScoped<IDataProvider, DataProvider>();
+                    break;
+                case (int)ServiceLifetime.Singleton:
+                    services.AddSingleton<IDataProvider, DataProvider>();
+                    break;
+            }
+        }
     }
 }
