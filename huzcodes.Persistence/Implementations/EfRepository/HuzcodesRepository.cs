fbad987ce3cefd6fd82c0c391a45b5ac4798a6f6
huzcodes <huzcodes@outlook.com>
2024-04-05 04:21:38 +0400

    adding combined/grouping the needed interfaces together for using ardalis specifications and entity framework core from one class, by inheriting it and passing the app db context we are good to go with repository pattern

---
+++ huzcodes.Persistence/Implementations/EfRepository/HuzcodesRepository.cs
@@ -0,0 +1,19 @@
+﻿using Ardalis.Specification.EntityFrameworkCore;
+using huzcodes.Persistence.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace huzcodes.Persistence.Implementations.EfRepository
+{
+    /// <summary>
+    /// This class is used to be inherited and passing the app db context of the solution to its own base class,
+    /// To be able to use IRepository interface that uses in its own implementation EF core, 
+    /// Ardalis.Specifications and Ardalis.EntityFrameWorkCore
+    /// </summary>
+    /// <typeparam name="TEntity">Entity data model as generic type</typeparam>
+    public abstract class HuzcodesRepository<TEntity> : RepositoryBase<TEntity>, IReadRepository<TEntity>, IRepository<TEntity> where TEntity : class
+    {
+        public HuzcodesRepository(DbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
