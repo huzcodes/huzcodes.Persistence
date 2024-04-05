c7740dcb6a8ac4a652b2d4347298260c0a87fd89
huzcodes <huzcodes@outlook.com>
2024-04-05 04:18:57 +0400

    adding interface for i read repository that inherits ardalis specification package

---
+++ huzcodes.Persistence/Interfaces/Repositories/IReadRepository.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Specification;
+
+namespace huzcodes.Persistence.Interfaces.Repositories
+{
+    /// <summary>
+    /// This interface is used for reading entities into SQL db using EF core and Ardalis.Specifications
+    /// </summary>
+    /// <typeparam name="TEntity">Entity data model as generic type</typeparam>
+    public interface IReadRepository<TEntity> : IReadRepositoryBase<TEntity> where TEntity : class
+    {
+    }
+}
