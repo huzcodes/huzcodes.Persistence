195fe247064c4d53692f1e471fbc168b570d7bee
huzcodes <huzcodes@outlook.com>
2024-04-05 04:19:49 +0400

    adding i repository interface for creating and inherits aradalis specifications to put all the uses together

---
+++ huzcodes.Persistence/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using Ardalis.Specification;
+
+namespace huzcodes.Persistence.Interfaces.Repositories
+{
+    /// <summary>
+    /// This interface is used for saving entities into SQL db using EF core and Ardalis.Specifications
+    /// </summary>
+    /// <typeparam name="TEntity">Entity data model as generic type</typeparam>
+    public interface IRepository<TEntity> : IRepositoryBase<TEntity> where TEntity : class
+    {
+    }
+
+}
