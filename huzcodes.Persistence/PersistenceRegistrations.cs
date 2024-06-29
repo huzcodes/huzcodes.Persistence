b27e62f0be5c83deca9f676e098fd751ebea62be
huzcodes <huzcodes@outlook.com>
2024-06-29 18:02:18 +0400

    - Adding new service registrations for mongo implementation and usage

--- huzcodes.Persistence/PersistenceRegistrations.cs
+++ huzcodes.Persistence/PersistenceRegistrations.cs
@@ -1,5 +1,6 @@
 ﻿using huzcodes.Persistence.Implementations;
 using huzcodes.Persistence.Interfaces;
+using huzcodes.Persistence.Interfaces.Mongo;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace huzcodes.Persistence
@@ -8,14 +9,14 @@ namespace huzcodes.Persistence
     {
         /// <summary>
         /// This function to be called in the program cs class,
-        /// for adding registration for the persistence services.
+        /// for adding registration for the persistence services of <strong>Dapper</strong>.
         /// Note: the service life time by default is scoped,
         /// and in case you want to change the service life time, 
         /// you can pass the int paramter serviceLifetime.
         /// </summary>
         /// <param name="serviceLifetime">parameter to define the service life time by deafult scoped
         /// ex: (int)ServiceLifetime.Scoped, (int)ServiceLifetime.Transient, (int)ServiceLifetime.Singleton</param>
-        public static void AddPersistenceServices(this IServiceCollection services, int serviceLifetime = (int)ServiceLifetime.Scoped)
+        public static void AddDapperPersistence(this IServiceCollection services, int serviceLifetime = (int)ServiceLifetime.Scoped)
         {
             switch (serviceLifetime)
             {
@@ -27,6 +28,31 @@ namespace huzcodes.Persistence
                     break;
                 case (int)ServiceLifetime.Singleton:
                     services.AddSingleton<IDataProvider, DataProvider>();
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// This function to be called in the program cs class,
+        /// for adding registration for the persistence services of <strong>Mongo</strong>.
+        /// Note: the service life time by default is scoped,
+        /// and in case you want to change the service life time, 
+        /// you can pass the int paramter serviceLifetime.
+        /// </summary>
+        /// <param name="serviceLifetime">parameter to define the service life time by deafult scoped
+        /// ex: (int)ServiceLifetime.Scoped, (int)ServiceLifetime.Transient, (int)ServiceLifetime.Singleton</param>
+        public static void AddMongoPersistence(this IServiceCollection services, int serviceLifetime = (int)ServiceLifetime.Scoped)
+        {
+            switch (serviceLifetime)
+            {
+                case (int)ServiceLifetime.Transient:
+                    services.AddTransient(typeof(IMongoProvider<>), typeof(IMongoProvider<>));
+                    break;
+                case (int)ServiceLifetime.Scoped:
+                    services.AddScoped(typeof(IMongoProvider<>), typeof(IMongoProvider<>));
+                    break;
+                case (int)ServiceLifetime.Singleton:
+                    services.AddSingleton(typeof(IMongoProvider<>), typeof(IMongoProvider<>));
                     break;
             }
         }
