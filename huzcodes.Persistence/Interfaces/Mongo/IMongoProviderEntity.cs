6a4f9bb358564adc966beff2e11ef7b8b60acc53
huzcodes <huzcodes@outlook.com>
2024-06-29 18:01:46 +0400

    - Adding new interface for mongo marking entity.

---
+++ huzcodes.Persistence/Interfaces/Mongo/IMongoProviderEntity.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using Newtonsoft.Json;
+
+namespace huzcodes.Persistence.Interfaces.Mongo
+{
+    public interface IMongoProviderEntity
+    {
+        /// <summary>
+        /// Id property, object type id for mongo identifier BsonType
+        /// </summary>
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        [JsonProperty("id")]
+        public string? Id { get; set; }
+    }
+}
