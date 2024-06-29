16719f8c0cc6290293fd4a6b814f6930ef9982c4
huzcodes <huzcodes@outlook.com>
2024-06-29 20:37:41 +0400

    - Adding new document collection class.

---
+++ huzcodes.Persistence.API/Models/MongoDocumentTest.cs
@@ -0,0 +1,24 @@
+﻿using huzcodes.Persistence.Interfaces.Mongo;
+using Newtonsoft.Json;
+
+namespace huzcodes.Persistence.API.Models
+{
+    public class MongoDocumentTest : IMongoProviderEntity
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [JsonProperty("description")]
+        public string Description { get; set; } = string.Empty;
+
+        [JsonProperty("major")]
+        public string Major { get; set; } = string.Empty;
+
+        public MongoDocumentTest(MongoDataModel dataModel)
+        {
+            Name = dataModel.Name;
+            Description = dataModel.Description;
+            Major = dataModel.Major;
+        }
+    }
+}
