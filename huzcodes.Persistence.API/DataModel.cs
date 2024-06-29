9588778f5544fb2b2754eb88b85df6fe837747b9
huzcodes <huzcodes@outlook.com>
2024-06-29 20:38:21 +0400

    - Adding request and response classes for mongo apis

--- huzcodes.Persistence.API/DataModel.cs
+++ huzcodes.Persistence.API/DataModel.cs
@@ -1,3 +1,5 @@
+using huzcodes.Persistence.API.Models;
+
 namespace huzcodes.Persistence.API
 {
     public class DataModel
@@ -10,5 +12,25 @@ namespace huzcodes.Persistence.API
         public decimal Price => 32 + (int)(PriceWithoutTax / 0.5556);
 
         public string ProductName { get; set; } = string.Empty;
+    }
+
+    public class MongoDataModel
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Major { get; set; } = string.Empty;
+    }
+
+    public class MongoDataModelResponse : MongoDataModel
+    {
+        public string Id { get; set; }
+
+        public MongoDataModelResponse(MongoDocumentTest dataModel)
+        {
+            Id = dataModel.Id;
+            Name = dataModel.Name;
+            Description = dataModel.Description;
+            Major = dataModel.Major;
+        }
     }
 }
